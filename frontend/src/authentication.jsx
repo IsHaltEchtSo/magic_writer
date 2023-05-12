92a9c2e8270b0ee65d2c70f144f454f0ecdeb331
IsHaltEchtSo <58997348+IsHaltEchtSo@users.noreply.github.com>
2023-05-12 13:50:47 +0200

    authentication

--- frontend/src/authentication.jsx
+++ frontend/src/authentication.jsx
@@ -4,6 +4,8 @@ export default function AuthenticationPage() {
     return (
         <>
             <NavigationBar />
+            <h1>Authentication</h1>
+            <p>This shows either a login or register link or is used to logout immediately</p>
         </>
     )
 }
