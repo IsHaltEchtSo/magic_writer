587e3beb6e89eff376db3d0f010371c06cc6fd42
IsHaltEchtSo <58997348+IsHaltEchtSo@users.noreply.github.com>
2023-05-12 13:44:48 +0200

    Added 'Edit' Mode

--- frontend/src/writer.jsx
+++ frontend/src/writer.jsx
@@ -1,10 +1,15 @@
+import  {Link } from 'react-router-dom'
+
 import NavigationBar from "./components/navbar";
 
 export default function WriterPage() {
     return (
         <>
             <NavigationBar />
-            <h1>Magic Writer</h1>
+            <h1>Magic Writer/Editor</h1>
+            <textarea></textarea>
+            <br />
+            <Link to='/writer'>Toggle</Link>
         </>
     )
 }
