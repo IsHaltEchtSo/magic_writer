55d4d8510e1ec48f4842547b9e851a1b3f95c2fb
IsHaltEchtSo <58997348+IsHaltEchtSo@users.noreply.github.com>
2023-05-13 11:55:45 +0200

    Replace non-Whitespace with Asterics

--- frontend/src/components/magicarea.jsx
+++ frontend/src/components/magicarea.jsx
@@ -1,35 +1,36 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 
 export default function MagicArea() {
-    // const specialChars = [
-    //     '\n', '\t', ' '
-    // ]
-    // const [text, setText] = useState('This is where you start your journey!')
+    const replacementCharacters = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        '1', '2', '3', '4', '5', '6', '7', '8', '9', '@'   
+    ]
+    const [text, setText] = useState('This is where you start your journey!')
 
-    // const handleChange = (event) => {
-    //     setText(event.target.value)
-    // }
+    const handleChange = (event) => {
+        setText(event.target.value)
+    }
 
-    // function magicallyDisplay(text) {
-    //     let magicText = ""
-    //     for (let i = 0; i < text.length; i++) {
-    //         if (text) {
-    //             console.log("new line detected")
-    //         } else {
-    //             magicText += "*"
-    //         }
-    //     }
-    //     return magicText
-    // }
+    function magicallyDisplay(text) {
+        let magicText = ""
+        for (let i = 0; i < text.length; i++) {
+            if (replacementCharacters.includes(text[i])) {
+                magicText += "*"
+            } else {
+                magicText += text[i]
+            }
+        }
+        return magicText
+    }
 
     return (
         <>
             <textarea 
                 className="magicArea"
-                // onChange={handleChange}
-                value='This is where you start your journey!'>
-                
-            </textarea>
+                onChange={handleChange}
+                value={magicallyDisplay(text)} 
+            />
         </>
     )
 }
