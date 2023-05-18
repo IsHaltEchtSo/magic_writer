5048414d3ac15ccd559b32b63dd90d8798d2d388
IsHaltEchtSo <58997348+IsHaltEchtSo@users.noreply.github.com>
2023-05-18 18:52:53 +0200

    Insert and Delete at Cursor Position

--- frontend/src/components/textarea.jsx
+++ frontend/src/components/textarea.jsx
@@ -5,29 +5,29 @@ import { useState, useEffect } from 'react'
 
 
 export default function TextArea({ textStyles, mode }) {
-    const [text, setText] = useState(localStorage.getItem('text') || 'This is where you start your journey!')
-    const [selectedText, setSelectedText] = useState('')
-    const [cursorPosition, setCursorPosition] = useState({'start': 0, 
-                                                          'end': 0 })
+  const [text, setText] = useState(localStorage.getItem('text') || 'This is where you start your journey!')
+  const [selectedText, setSelectedText] = useState('')
+  const [cursorPosition, setCursorPosition] = useState({'start': 0, 
+                                                        'end': 0 })
 
-    // Update the LocalStorage when the 'text'-State changes
-    useEffect(() => {
-        localStorage.setItem('text', text)
-    }, [text])
+  // Update the LocalStorage when the 'text'-State changes
+  useEffect(() => {
+    localStorage.setItem('text', text)
+  }, [text])
 
-    // Displays the input Text in its asterisked Version
-    function magicallyDisplay(text) {
-        let magicText = ""
+  // Displays the input Text in its asterisked Version
+  function magicallyDisplay(text) {
+    let magicText = ""
 
-        for (let i = 0; i < text.length; i++) {
-            if (specialCharacters.includes(text[i])){
-                magicText += text[i]
-            } else {
-                magicText += "*"
-            }
-        }
+    for (let i = 0; i < text.length; i++) {
+      if (specialCharacters.includes(text[i])){
+        magicText += text[i]
+      } else {
+        magicText += "*"
+      }
+    }
 
-        return magicText
+      return magicText
     }
 
     // Updates the currently selected Text
@@ -39,68 +39,79 @@ export default function TextArea({ textStyles, mode }) {
       setSelectedText(text.substring(cursorStart, cursorEnd))
     }
 
-    const handleChange = (event) => {
-        switch (event.nativeEvent.inputType) {
-            case eventTypes.insertChar:
-                setText(text.concat(event.target.value[event.target.value.length - 1]))
-                break
-            case eventTypes.deleteChar:
-                setText(text.substring(0, event.target.value.length))
-                break
-            case eventTypes.deleteLine:
-                setText(text.substring(0, event.target.value.length))
-                break
-            case eventTypes.insertLine:
-                setText(text.concat(event.target.value[event.target.value.length - 1]))
-                break
-            // default:
-            //     console.log('Another event is being detected')
-            //     console.log(event)
-        }
-    }
+  const handleChange = (event) => {
+    let leftString, rightString = ['', '']
 
-    const handleCut = () => {
-      navigator.clipboard
-        .writeText(selectedText)
-        .then(() => {
-          let [leftString, rightString] = [text.substring(0, cursorPosition.start), text.substring(cursorPosition.end, text.length)]
+    switch (event.nativeEvent.inputType) {
+      case eventTypes.insertChar:
+        [leftString, rightString] = [text.substring(0, cursorPosition.start), text.substring(cursorPosition.end, text.length)]
+        let insertedCharacter = event.nativeEvent.data
+        setText(leftString + insertedCharacter + rightString)
+        break
+      case eventTypes.deleteChar:
+        if (selectedText) {
+          leftString = text.substring(0, cursorPosition.start)
+          rightString = text.substring(cursorPosition.end, text.length)
           setText(leftString + rightString)
-        })
+        } else {
+          leftString = text.substring(0, cursorPosition.start - 1)
+          rightString = text.substring(cursorPosition.end, text.length)
+          setText(leftString + rightString)
+        }
+        break
+      case eventTypes.deleteLine:
+        // !! Needs to be implemented
+        break
+      case eventTypes.insertLine:
+        [leftString, rightString] = [text.substring(0, cursorPosition.start), text.substring(cursorPosition.end, text.length)]
+        setText(leftString + "\n" + rightString)
+        break
     }
+  }
 
-    const handlePaste = () => {
-      let additionalText = ''
-        navigator.clipboard
-          .readText()
-          .then((data) => {
-            additionalText = data
-            setText(text.concat(additionalText))
-          })
-    }
+  const handleCut = () => {
+  navigator.clipboard
+    .writeText(selectedText)
+    .then(() => {
+      let [leftString, rightString] = [text.substring(0, cursorPosition.start), text.substring(cursorPosition.end, text.length)]
+      setText(leftString + rightString)
+    })
+  }
+
+  const handlePaste = () => {
+  let additionalText = ''
+  navigator.clipboard
+    .readText()
+    .then((data) => {
+      additionalText = data
+      setText(text.concat(additionalText))
+    })
+  }
 
     if (mode === modes.edit) {
-        return (
-            <textarea 
-                className="magic-area"
-                id='magic-area'
-                onChange={handleChange}
-                onCut={handleCut}
-                onPaste={handlePaste}
-                style={{'fontStyle': textStyles.fontStyle, 
-                        'fontWeight': textStyles.fontWeight, 
-                        'textDecoration': textStyles.textDecoration }}
-                onSelect={handleSelect}
-                value={text} 
-            />
-        )
+      return (
+        <textarea
+          className="magic-area"
+          id='magic-area'
+          onInput={handleChange}
+          onCut={handleCut}
+          onPaste={handlePaste}
+          style={{'fontStyle': textStyles.fontStyle, 
+                  'fontWeight': textStyles.fontWeight, 
+                  'textDecoration': textStyles.textDecoration }}
+          onSelect={handleSelect}
+          value={text} 
+        />
+      )
     } else if (mode === modes.write) {
-        return (
-            <textarea 
-                className="magic-area"
-                id='magic-area'
-                onChange={handleChange}
-                value={magicallyDisplay(text)} 
-            />
-        )
+      return (
+        <textarea
+          className="magic-area"
+          id='magic-area'
+          onChange={handleChange}
+          value={magicallyDisplay(text)} 
+          onSelect={handleSelect}
+        />
+      )
     } 
 }
