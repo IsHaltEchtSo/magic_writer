fae2c560d74b299877579a641a2b1c64050e4349
IsHaltEchtSo <58997348+IsHaltEchtSo@users.noreply.github.com>
2023-05-18 13:10:01 +0200

    Using the Clipboard to CMD C/X/V

--- frontend/src/components/textarea.jsx
+++ frontend/src/components/textarea.jsx
@@ -6,6 +6,9 @@ import { useState, useEffect } from 'react'
 
 export default function TextArea({ textStyles, mode }) {
     const [text, setText] = useState(localStorage.getItem('text') || 'This is where you start your journey!')
+    const [selectedText, setSelectedText] = useState('')
+    const [cursorPosition, setCursorPosition] = useState({'start': 0, 
+                                                          'end': 0 })
 
     // Update the LocalStorage when the 'text'-State changes
     useEffect(() => {
@@ -27,6 +30,15 @@ export default function TextArea({ textStyles, mode }) {
         return magicText
     }
 
+    // Updates the currently selected Text
+    const handleSelect = () => {
+      const textarea = document.getElementById('magic-area')
+      const [cursorStart, cursorEnd] = [textarea.selectionStart, textarea.selectionEnd]
+      
+      setCursorPosition({'start': cursorStart, 'end': cursorEnd})
+      setSelectedText(text.substring(cursorStart, cursorEnd))
+    }
+
     const handleChange = (event) => {
         switch (event.nativeEvent.inputType) {
             case eventTypes.insertChar:
@@ -41,20 +53,43 @@ export default function TextArea({ textStyles, mode }) {
             case eventTypes.insertLine:
                 setText(text.concat(event.target.value[event.target.value.length - 1]))
                 break
-            default:
-                console.log('Another event is being detected')
-                console.log(event)
+            // default:
+            //     console.log('Another event is being detected')
+            //     console.log(event)
         }
+    }
+
+    const handleCut = () => {
+      navigator.clipboard
+        .writeText(selectedText)
+        .then(() => {
+          let [leftString, rightString] = [text.substring(0, cursorPosition.start), text.substring(cursorPosition.end, text.length)]
+          setText(leftString + rightString)
+        })
+    }
+
+    const handlePaste = () => {
+      let additionalText = ''
+        navigator.clipboard
+          .readText()
+          .then((data) => {
+            additionalText = data
+            setText(text.concat(additionalText))
+          })
     }
 
     if (mode === modes.edit) {
         return (
             <textarea 
                 className="magic-area"
+                id='magic-area'
                 onChange={handleChange}
+                onCut={handleCut}
+                onPaste={handlePaste}
                 style={{'fontStyle': textStyles.fontStyle, 
                         'fontWeight': textStyles.fontWeight, 
                         'textDecoration': textStyles.textDecoration }}
+                onSelect={handleSelect}
                 value={text} 
             />
         )
@@ -62,6 +97,7 @@ export default function TextArea({ textStyles, mode }) {
         return (
             <textarea 
                 className="magic-area"
+                id='magic-area'
                 onChange={handleChange}
                 value={magicallyDisplay(text)} 
             />
